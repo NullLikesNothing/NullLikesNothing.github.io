41a75aa7176e06ac8d4591c215af1736444b3560
NullLikesNothing <82908498+NullLikesNothing@users.noreply.github.com>
2022-03-07 11:52:59 -0600

    Update shw.js

--- shw.js
+++ shw.js
@@ -1,16 +1,12 @@
 let tabs = [];
+let users = [];
 let messages = [];
 let cap = {
   msg: 'MSG',
   dc: 'DC',
   reg: 'REG',
-  msgs: 'MSGS',
+  clr: 'CLR',
   regerr: 'REGERR',
-}
-
-function removeVal(arr,val) {
-  if (arr.indexOf(val) === -1) return arr
-  return arr.splice(arr.indexOf(val), 1)
 }
 
 onconnect = function(e) {
@@ -21,10 +17,35 @@ onconnect = function(e) {
   port.onmessage = function(ev) {
     const data = ev.data;
     
-    messages.push(data)
+    if(data[0] === cap.reg) {
+      if (data[1] in users) {
+        port.postMessage([cap.regerr,`User "${data[1]}" in use.`])
+      } else {
+        messages.push(`User "${data[1]}" has connected.`)
+        users.push(data[1])
+      }
+    } else if(data[0] === cap.msg) {
+      if (data[1].startsWith('@!')) {
+        let cmd = data[1].replace('@!','')
+        let args = cmd.split(' ')
+        args = args.splice(1,args.length-1)
+        messages.push(`#${user[tabs.indexOf(port)]}`)
+        return
+      }
+      messages.push(`#${users[tabs.indexOf(port)]}: ${data[1]}`)
+    } else if(data[0] === cap.dc) {
+      messages.push(`User ${users[tabs.indexOf(port)]} has disconnected.`)
+      users.splice(tabs.indexOf(port),1)
+      tabs.splice(tabs.indexOf(port),1)
+    } else if(data[0] === cap.clr) {
+      messages.length = 0
+    } else {
+      port.postMessage([cap.regerr,`Invalid command ${data[0]}.`])
+      return
+    }
     
     tabs.forEach(instance => {
-      instance.postMessage([cap.msgs,messages]);
+      instance.postMessage([cap.msg,messages]);
     });
   }
 }
