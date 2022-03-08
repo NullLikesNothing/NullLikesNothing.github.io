f7024ba25bf40d0668ea7215d203ed981d86406f
NullLikesNothing <82908498+NullLikesNothing@users.noreply.github.com>
2022-03-07 18:59:16 -0600

    Update shw.js

--- shw.js
+++ shw.js
@@ -1,58 +1,86 @@
+/* TODO:
+ * finish bans and unbans.
+ * add NOP code
+ * fix bugs
+ */
+
 let tabs = [];
 let users = [];
 let messages = [];
+let banned = [];
 const cap = {
   msg: 'MSG',
   dc: 'DC',
   reg: 'REG',
+  sreg: 'SREG',
   clr: 'CLR',
   regerr: 'REGERR',
   ref: 'REF',
+  ban: 'BAN',
+  unban: 'UNBAN',
+  nop: 'NOP',
 }
+let banInt = setInterval(() => {
+  banned.forEach(usr => {
+    if (users.indexOf(usr) === -1) return
+    tabs[users.indexOf(usr)].postMessage(["REGERR", "BANNED"])
+    users[users.indexOf(usr)] = null
+  })
+}, 100)
 
 onconnect = function(e) {
   const port = e.ports[0]
 
   tabs.push(port)
-  
+
   port.onmessage = function(ev) {
     const data = ev.data;
-    
-    if(data[0] === cap.reg) {
-      if (data[1] in users) {
-        port.postMessage([cap.regerr,`User "${data[1]}" in use.`])
-      } else {
-        messages.push(`User "${data[1]}" has connected.`)
+
+    if (data[0] === cap.reg || data[0] === cap.sreg) {
+      if (users.indexOf(data[1]) === -1) {
+        if (data[0] === cap.reg) messages.push(`User "${data[1]}" has connected.`)
         users[tabs.indexOf(port)] = data[1]
+      } else {
+        port.postMessage([cap.regerr, `User "${data[1]}" in use.`])
       }
-    } else if(data[0] === cap.msg) {
+    } else if (data[0] === cap.msg) {
       messages.push(`#${users[tabs.indexOf(port)]}: ${data[1]}`)
-    } else if(data[0] === cap.dc) {
+    } else if (data[0] === cap.dc) {
       if (users[tabs.indexOf(port)] == undefined) {
-        tabs.splice(tabs.indexOf(port),1)
+        tabs.splice(tabs.indexOf(port), 1)
         return
       }
       messages.push(`User "${users[tabs.indexOf(port)]}" has disconnected.`)
-      users.splice(tabs.indexOf(port),1)
-      tabs.splice(tabs.indexOf(port),1)
-    } else if(data[0] === cap.clr) {
+      users.splice(tabs.indexOf(port), 1)
+      tabs.splice(tabs.indexOf(port), 1)
+    } else if (data[0] === cap.clr) {
       messages.length = 0
-    } else if(data[0] === cap.regerr) {
+    } else if (data[0] === cap.regerr) {
       let target = data[1]
       if (users.indexOf(target) === -1) return
       messages.push(`User "${target}" has disconnected.`)
-      tabs[users.indexOf(target)].postMessage([cap.regerr,"Internal error"])
-    } else if(data[0] === cap.ref) {
+      tabs[users.indexOf(target)].postMessage([cap.regerr, "Internal error"])
+      users[users.indexOf(target)] = null
+    } else if (data[0] === cap.ref) {
       tabs.forEach(instance => {
         instance.postMessage([cap.ref]);
       });
+    } else if (data[0] === cap.ban) {
+      let target_ban = data[1]
+      if (users.indexOf(target_ban) === -1) {
+        port.postMessage([cap.nop, `User ${target_ban} not found.`])
+        return
+      } else if (banned.indexOf(target_ban) !== -1) {
+        
+      }
+      banned.push()
     } else {
-      port.postMessage([cap.regerr,`Invalid command ${data[0]}.`])
+      port.postMessage([cap.nop, `Invalid command ${data[0]}.`])
       return
     }
-    
+
     tabs.forEach(instance => {
-      instance.postMessage([cap.msg,messages]);
+      instance.postMessage([cap.msg, messages]);
     });
   }
 }
