8eff9a5b2f5b6d2b5360c518e12dbc6365392e03
NullLikesNothing <82908498+NullLikesNothing@users.noreply.github.com>
2022-05-30 17:36:40 -0500

    Update shw.js

--- shw.js
+++ shw.js
@@ -6,8 +6,8 @@ let tabs = [];
 let users = [];
 let messages = [];
 let banned = [];
-let notadm = "You're not the admin."
-let admin = null;
+let notadm = "You're not an admin."
+let admin = [];
 const cap = {
   msg: 'MSG',
   dc: 'DC',
@@ -22,6 +22,7 @@ const cap = {
   sban: 'SBAN',
   unban: 'UNBAN',
   nop: 'NOP',
+  rnk: 'RNK',
 }
 let banInt = setInterval(() => {
   banned.forEach(usr => {
@@ -38,15 +39,35 @@ onconnect = function(e) {
 
   port.onmessage = function(ev) {
     const data = ev.data;
-    if (data[0] === cap.adm && (admin == null || admin == undefined)) {
-      admin = port
-    } else if (data[0] === cap.unadm && (admin != null || admin != undefined)) {
-      if (port !== admin) return port.postMessage([cap.nop, notadm])
-      admin = null
+    if (data[0] === cap.adm) {
+      let usr = data[1]
+      if (admin.length === 0) admin.push(port)
+      if (users.indexOf(usr) === -1) return port.postMessage([cap.nop, "User not found."])
+      let user = tabs[users.indexOf(usr)]
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
+      if (admin.indexOf(user) !== -1) return port.postMessage([cap.nop, "User is already admin!"])
+      admin.push(user)
+      user.postMessage([cap.rnk,true])
+      users[users.indexOf(usr)] = "[A] " + users[users.indexOf(usr)]
+    } else if (data[0] === cap.unadm) {
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
+      let usr = data[1]
+      if (admin.length === 0) return port.postMessage([cap.nop,"No admins!"])
+      if (users.indexOf(usr) === -1) return port.postMessage([cap.nop, "User not found."])
+      let user = tabs[users.indexOf(usr)]
+      if (admin.indexOf(user) === -1) return port.postMessage([cap.nop, "User is not admin!"])
+      admin.splice(admin.indexOf(user),1)
+      user.postMessage([cap.rnk,false])
+      users[users.indexOf(usr)] = users[users.indexOf(usr)].replace(["[A] ",""])
     } else if (data[0] === cap.reg || data[0] === cap.sreg) {
       if (data[1].trim() === "") return port.postMessage([cap.regerr, "Name cannot be blank or whitespace!"])
+      data[1] = data[1].replaceAll("[A] ", "")
       if (users.indexOf(data[1]) === -1) {
         if (banned.indexOf(data[1]) !== -1) return port.postMessage([cap.regerr,"BANNED"])
+        if (admin.indexOf(port) !== -1) {
+          data[1] = "[A] " + data[1]
+          port.postMessage([cap.rnk, true])
+        }
         if (data[0] === cap.reg) messages.push(`User "${data[1]}" has connected.`)
         users[tabs.indexOf(port)] = data[1]
       } else {
@@ -55,7 +76,9 @@ onconnect = function(e) {
     } else if (data[0] === cap.msg) {
       messages.push(`#${users[tabs.indexOf(port)]}: ${data[1]}`)
     } else if (data[0] === cap.dc) {
-      if (port === admin) admin = null
+      if (admin.indexOf(port) !== -1) {
+        admin.splice(admin.indexOf(port),1)
+      }
       if (users[tabs.indexOf(port)] == undefined) return tabs.splice(tabs.indexOf(port), 1)
       messages.push(`User "${users[tabs.indexOf(port)]}" has disconnected.`)
       users.splice(tabs.indexOf(port), 1)
@@ -63,19 +86,19 @@ onconnect = function(e) {
     } else if (data[0] === cap.clr) {
       messages.length = 0
     } else if (data[0] === cap.regerr) {
-      if (port !== admin) return port.postMessage([cap.nop, notadm])
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
       let target = data[1]
       if (users.indexOf(target) === -1) return
       messages.push(`User "${target}" has disconnected.`)
       tabs[users.indexOf(target)].postMessage([cap.regerr, "Internal error"])
       users[users.indexOf(target)] = null
     } else if (data[0] === cap.ref) {
-      if (port !== admin) return port.postMessage([cap.nop, notadm])
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
       tabs.forEach(instance => {
         instance.postMessage([cap.ref]);
       });
     } else if (data[0] === cap.ban || data[0] === cap.sban ) {
-      if (port !== admin) return port.postMessage([cap.nop, notadm])
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
       let target_ban = data[1]
       if (users.indexOf(target_ban) === -1) return port.postMessage([cap.nop, `User "${target_ban}" not found.`])
       if (banned.indexOf(target_ban) !== -1) return
@@ -83,13 +106,13 @@ onconnect = function(e) {
       port.postMessage([cap.nop, `User "${target_ban}" banned.`])
       if (data[0] === cap.ban) messages.push(`User "${target_ban}" has disconnected`)
     } else if (data[0] === cap.unban) {
-      if (port !== admin) return port.postMessage([cap.nop, notadm])
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
       let target_unban = data[1]
       if (banned.indexOf(target_unban) === -1) return port.postMessage([cap.nop, `User "${target_unban}" not found.`])
       banned.splice(banned.indexOf(target_unban),1)
       port.postMessage([cap.nop, `User "${target_unban}" unbanned.`])
     } else if (data[0] === cap.nop) {
-      if (port !== admin) return port.postMessage([cap.nop, notadm])
+      if (admin.indexOf(port) === -1) return port.postMessage([cap.nop, notadm])
       try {
         port.postMessage([cap.nop,eval(data[1])])
       } catch (e) {
